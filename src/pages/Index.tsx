b2a90644d7033589fa177ef04868a2745a40ba9c
Mosth2026 <261988695+Mosth2026@users.noreply.github.com>
2026-03-15 07:15:52 +0200

    remove trust banner component and its usage from home page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,7 +7,7 @@ import Features from "@/components/Features";
 import Testimonials from "@/components/Testimonials";
 import Newsletter from "@/components/Newsletter";
 import LuxuryExperience from "@/components/LuxuryExperience";
-import TrustBanner from "@/components/TrustBanner";
+
 import SocialBanner from "@/components/SocialBanner";
 import { Helmet } from "react-helmet-async";
 
@@ -61,7 +61,7 @@ const Index = () => {
         <Header />
         <main className="flex-grow">
           <Hero />
-          <TrustBanner />
+
           <FeaturedProducts />
           <Features />
           <SocialBanner />
